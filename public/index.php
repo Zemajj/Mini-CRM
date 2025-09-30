b0793090340cbf6a492408812eef496f6cd7b4f7
Zemma <semyon.smirnov.7@gmail.com>
2025-09-30 21:06:12 +1000

    Проверка работы метода all()

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 
 use app\Core\Database\Database;
+use app\Model\User;
 
-$db = Database::getConnection();
-
-var_dump($db);
+    $userModel = new User();
+    print_r($userModel->all());
