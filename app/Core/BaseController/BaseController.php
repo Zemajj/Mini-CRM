23c926b65548153e2bcf19e2ffdd6ea538e7f39f
zemma <semyon.smirnov.7@gmail.com>
2025-10-27 21:46:43 +1000

    Удалил заглушки данных классов.

--- app/Core/BaseController/BaseController.php
+++ app/Core/BaseController/BaseController.php
@@ -4,10 +4,6 @@ namespace app\Core\BaseController;
 
 class BaseController
 {
-    protected function render($view, $data = []): void
-    {
-        extract($data);
 
-        include __DIR__ . "/../View/$view.php";
-    }
+
 }
--- app/Core/Router/Router.php
+++ app/Core/Router/Router.php
@@ -8,27 +8,7 @@ namespace app\Core\Router;
 
 class Router
 {
-    protected array $routes = [];
 
-    public function addRoute($route, $controller, $action): void
-    {
-        $this->routes[$route] = ['controller' => $controller, 'action' => $action];
-    }
 
-    /**
-     * @throws \Exception
-     */
-    public function dispatch($uri): void
-    {
-        if (array_key_exists($uri, $this->routes)) {
-            $controller = $this->routes[$uri]['controller'];
-            $action = $this->routes[$uri]['action'];
-
-            $controller = new $controller();
-            $controller->$action();
-        } else {
-            throw new \RuntimeException("Route $uri not found");
-        }
-    }
 
 }
