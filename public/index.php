f344f73906f8934b7c33014698829b196a2d916b
Zemajj <semyon.smirnov.7@gmail.com>
2025-11-20 20:38:18 +1000

    Создал объект класса Roter, добавил маршруты

--- public/index.php
+++ public/index.php
@@ -1,9 +1,20 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+// подключение автозагрузки классов
+require_once __DIR__. '/../vendor/autoload.php';
 
 
 use app\Core\Database\Database;
 use app\Model\User;
 use app\Core\BaseController\BaseController;
+use app\Core\Router\Router;
+
+// Создание объекта роутер
+ $router = new Router();
+
+
+ // Регистрация маршрутов
+ $router->add('GET', '/', 'HomeController');
+ $router->add('POST', '/login', 'LoginController');
+
 
