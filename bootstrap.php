2b1ac93a1ba8831dc8674c35858a7a133d6c227d
Zema <semyon.smirnov.7@gmail.com>
2026-08-13 21:47:13 +1000

    feat: validate database environment configuration

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 use Dotenv\Dotenv;
 
@@ -7,8 +8,14 @@ require __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-/*
- *  В данном файле будет размещено подключение
- * файла .env и autoload
- *
- */
+$dotenv
+    ->required(['DB_HOST', 'DB_DATABASE', 'DB_USERNAME', 'DB_PASSWORD'])
+    ->notEmpty();
+$dotenv->required(['DB_PORT'])->notEmpty()->isInteger();
+
+$dbPort = $_ENV['DB_PORT'];
+$dbPort = (int)$dbPort;
+
+if ($dbPort < 1 || $dbPort > 65535) {
+    die("DB Port must be between 1 and 65535");
+}
